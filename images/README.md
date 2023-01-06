9272230208fd85ff7245dfbf0c3e9e48e7c23204
Thomas Draper <tgdrape@gmail.com>
2023-01-06 09:10:28 -0800

    Make sequence of Gosper curves.

---
+++ images/README.md
@@ -0,0 +1,1 @@
+# Making GIFs for github
---
+++ images/createGIF.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+convert GosperHexagon0.svg GosperHexagon1.svg GosperHexagon2.svg GosperHexagon3.svg GosperHexagon3.svg GosperHexagon2.svg GosperHexagon1.svg GosperHexagon0.svg -loop 0 -morph 19 gosper.gif
